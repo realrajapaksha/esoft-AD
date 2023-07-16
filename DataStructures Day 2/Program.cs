71a08af239493118aaf93ceb7bb3ae0365c8165f
cflkdeveloper <cflkdeveloper@gmail.com>
2023-07-16 10:36:55 +0530

    Data Structures Day 2
    Linked List with Queue
    2023-07-16

---
+++ DataStructures Day 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+
+            /*Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());*/
+            queue.Display();
+
+            queue.Dequeue();
+
+            queue.Enqueue(8);
+            queue.Display();
+            queue.Dequeue();
+            queue.Enqueue(9);
+
+            queue.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DataStructures Day 2/Queue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Day_2
+{
+    internal class Queue
+    {
+        Node front;
+        Node rear;
+        int size;
+
+        public Queue()
+        {
+            this.front = null;
+            this.rear = null;
+            this.size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            if(size == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Enqueue(int x)
+        {
+            Node node = new Node(x);
+            if(IsEmpty() )
+            {
+                front = rear = node;
+            }
+            else
+            {
+                rear.next = node;
+                rear = node;
+            }
+
+            size++;
+        }
+
+
+        public int Dequeue()
+        {
+            int x = -1;
+            if(IsEmpty())
+            {
+                Console.WriteLine("Empty Queue");
+            }
+            else
+            {
+                x = front.data;
+                front = front.next;
+                size--;
+            }
+            return x;
+        }
+
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty Queue");
+            }
+            else
+            {
+                Node temp = front;
+                while(temp != null)
+                {
+                    Console.WriteLine(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
