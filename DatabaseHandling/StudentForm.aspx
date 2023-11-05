371644e1fa249bc25f921e9e413547285f3dcdd6
cflkdeveloper <cflkdeveloper@gmail.com>
2023-11-05 11:15:06 +0530

    Database Handling ASP web

---
+++ DatabaseHandling/StudentForm.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StudentForm.aspx.cs" Inherits="DatabaseHandling.StudentForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 164px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table style="width: 100%;">
+            <tr>
+                <td class="auto-style1">Name</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server" Width="317px"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td class="auto-style1">ID</td>
+                <td>
+                    <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtID" ErrorMessage="ID can't be empty" ForeColor="Red"></asp:RequiredFieldValidator>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Address</td>
+                <td>
+                    <asp:TextBox ID="txtAddress" runat="server" Width="312px" Height="49px"></asp:TextBox></td>
+                <td>&nbsp;</td>
+            </tr>
+
+        </table>
+        <tr>
+            <td>
+                <asp:Button ID="AddBtn" runat="server" Text="ADD" OnClick="AddBtn_Click" />
+            </td>
+            <td>
+                 <asp:Button ID="UpdateBtn" runat="server" Text="Update" OnClick="UpdateBtn_Click" />
+            <td>
+                <asp:Button ID="FindBtn" runat="server" Text="Find" OnClick="FindBtn_Click" />
+            <td>
+                <asp:Button ID="DeleteBtn" runat="server" Text="Delete" OnClick="DeleteBtn_Click" />
+            <td>
+               <asp:Button ID="ClearBtn" runat="server" Text="Clear" OnClick="ClearBtn_Click" />
+        </tr>
+        <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ DatabaseHandling/StudentForm.aspx.cs
@@ -0,0 +1,168 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Web.UI;
+
+
+namespace DatabaseHandling
+{
+    public partial class StudentForm : System.Web.UI.Page
+    {
+        public MySqlConnection dbCon;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+            MakeConnection();
+        }
+
+        private void MakeConnection()
+        {
+            try
+            {
+                MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+                conn_string.Server = "localhost";
+                conn_string.Port = 3306;
+                conn_string.UserID = "root";
+                conn_string.Password = "";
+                conn_string.Database = "first";
+
+                dbCon = new MySqlConnection(conn_string.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+        protected void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (ValidationCheck())
+            {
+                string quary = $"INSERT INTO `student`(`Name`, `Id`, `Address`) VALUES ('{txtName.Text}','{txtID.Text}','{txtAddress.Text}')";
+                MySqlCommand command = new MySqlCommand(quary, dbCon);
+
+                try
+                {
+                    dbCon.Open();
+
+                    command.ExecuteNonQuery();
+                    Response.Write($"Successfull Inserted.");
+                    // to refresh data gridview
+                    GridView1.DataBind();
+                    dbCon.Close();
+                }
+                catch (Exception ex)
+                {
+                    dbCon.Close();
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private bool ValidationCheck()
+        {
+            try
+            {
+                string quary = $"SELECT * FROM student WHERE id= '{txtID.Text}'";
+                MySqlCommand command = new MySqlCommand(quary, dbCon);
+
+                command.CommandTimeout = 60;
+
+                dbCon.Open();
+                MySqlDataReader data = command.ExecuteReader();
+                if (data.HasRows)
+                {
+                    Response.Write($"{txtID.Text} id already exists.");
+                    dbCon.Close();
+                    return false;
+                }
+                else
+                {
+                    dbCon.Close();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                dbCon.Close();
+                Response.Write($"error {ex}");
+                return false;
+            }
+        }
+
+        protected void ClearBtn_Click(object sender, EventArgs e)
+        {
+            txtName.Text = string.Empty;
+            txtID.Text = string.Empty;
+            txtAddress.Text = string.Empty;
+        }
+
+        protected void FindBtn_Click(object sender, EventArgs e)
+        {
+            string quary = $"SELECT Name, Id, Address FROM student WHERE id='{txtID.Text}'";
+            MySqlCommand command = new MySqlCommand(quary, dbCon);
+            command.CommandTimeout = 60;
+
+            try
+            {
+                dbCon.Open();
+                MySqlDataReader data = command.ExecuteReader();
+                if (data.HasRows)
+                {
+                    if (data.Read())
+                    {
+                        txtName.Text = data.GetString(0);
+                        txtID.Text = data.GetString(1);
+                        txtAddress.Text = data.GetString(2);
+                    }
+                    dbCon.Close();
+                }
+                else
+                {
+                    Response.Write($"{txtID.Text} id not found.");
+                    dbCon.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                dbCon.Close();
+                Response.Write($"error {ex}");
+            }
+        }
+
+        protected void UpdateBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            string quary = $"DELETE FROM student WHERE id='{txtID.Text}'";
+            MySqlCommand command = new MySqlCommand(quary, dbCon);
+            command.CommandTimeout = 60;
+
+            try
+            {
+                dbCon.Open();
+                int resut = command.ExecuteNonQuery();
+                if (resut <= 0)
+                {
+                    Response.Write($"{txtID.Text} id not found.");
+                }
+                else
+                {
+                    Response.Write($"{txtID.Text} id delete succesfull.");
+                }
+                // to refresh data gridview
+                GridView1.DataBind();
+                dbCon.Close();
+
+            }
+            catch (Exception ex)
+            {
+                dbCon.Close();
+                Response.Write($"error {ex}");
+            }
+        }
+    }
+}
